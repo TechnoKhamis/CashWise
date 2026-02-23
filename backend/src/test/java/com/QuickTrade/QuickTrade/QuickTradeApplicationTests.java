c858503c313d6e330132178efbbbb644dce8dc1e
Salman Khamis <salman.khamis@array.world>
2026-02-23 13:58:20 +0300

    backend skeleton

---
+++ backend/src/test/java/com/QuickTrade/QuickTrade/QuickTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuickTrade.QuickTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
